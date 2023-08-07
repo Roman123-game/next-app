4ca7e3e73ae735421f0ee085854b0723511cbca9
RB <82284311+Roman123-game@users.noreply.github.com>
2023-08-07 15:33:54 +0300

    chore: delete Box.jsx and app/index.jsx

---
+++ app/index.js
@@ -0,0 +1,38 @@
+'use client'
+
+import { Canvas,} from '@react-three/fiber'
+import Pyramid from './componenets/Box.js'
+import {useState } from 'react'
+
+export default function App() {
+  const [stateArr, setStateArr]= useState([])
+  const array =[]
+  function fun(){
+    array.push("a");
+  
+    setStateArr(array)
+    console.log(state)
+  }
+  function del(){
+    array.pop();
+    setStateArr(array)
+  }
+  return (
+    <div>
+    <Canvas>
+      <color attach="background" args={['#fff']} />
+      <ambientLight intensity={0.5} />      
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />      
+      <pointLight position={[-10, -10, -10]} /> 
+       {stateArr.map((item,index)=> {return  <Pyramid position={[index, 0, 0]} color={'red'} />})}
+       <Pyramid position={[-1.2, 0, 0]} color={'red'} />
+
+   
+      </Canvas>
+    <button onClick={()=>fun()}>add</button>
+    <button onClick={()=>del()}>del</button>
+
+    </div>
+    
+  )
+}
